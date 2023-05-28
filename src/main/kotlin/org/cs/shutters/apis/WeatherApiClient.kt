9173a136a646973578f3a8f3ef639bb4d0e9554f
cseewald <kontakt@christian-seewald.com>
2023-05-29 00:27:09 +0200

    Fix WeatherApiClient

--- src/main/kotlin/org/cs/shutters/apis/WeatherApiClient.kt
+++ src/main/kotlin/org/cs/shutters/apis/WeatherApiClient.kt
@@ -3,7 +3,6 @@ package org.cs.shutters.apis
 import com.fasterxml.jackson.annotation.JsonProperty
 import kotlinx.coroutines.runBlocking
 import org.cs.shutters.ShuttersProperties
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
@@ -15,7 +14,6 @@ class WeatherApiClient(
 ) {
     private val webClient = webClientBuilder.baseUrl("https://api.weatherapi.com").build()
 
-    @Cacheable("weather")
     fun getCurrentWeather() = runBlocking {
         return@runBlocking webClient.get()
             .uri("/v1/current.json?key=${shuttersProperties.apiWebclients.weatherApi.key}&q=${shuttersProperties.latitude},${shuttersProperties.longitude}&aqi=no")
